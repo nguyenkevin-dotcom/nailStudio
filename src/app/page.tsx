7523fb73491a620e6d2c6df1af9c61581038e0f8
Kevin Nguyen <kevinlat6@gmail.com>
2025-06-09 16:54:40 +0000

    i would like the font to be white and time period must be from 8:00AM to

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,8 +15,8 @@ const availableServices: Service[] = [
 ];
 
 const timeSlots = [
-  '09:00 AM', '10:00 AM', '11:00 AM', '12:00 PM', 
-  '01:00 PM', '02:00 PM', '03:00 PM', '04:00 PM', '05:00 PM'
+  '08:00', '09:00', '10:00', '11:00', '12:00', 
+  '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00'
 ];
 
 export default function HomePage() {
